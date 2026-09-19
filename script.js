3b1498b465d9308d7e424195edf240e798ff7f87
HungryHippoNo3 <157853215+HungryHippoNo3@users.noreply.github.com>
2026-09-18 19:17:42 -0500

    Rewrote original hint code

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ window.addEventListener("DOMContentLoaded", () => {
         .then(text => {
             const dictionary = JSON.parse(text);
 
-        const randomRow = dictionary[Math.floor(Math.random() * dictionary.length)];
-        const noun = randomRow[0];
+        const randomRowIndex = 1 + Math.floor(Math.random() * (dictionary.length - 1));
+        const noun = dictionary[randomRowIndex][0];
 
-        const language1 = randomRow[20, 21, 22][Math.floor(Math.random() * 3)];
+        const language1Index = (dictionary.length() - 3) + Math.floor(Math.random() * 3);
 
-        const hint1 = language1 + ": " + randomRow[language1];
+        const hint1 = dictionary[0][language1Index] + ": " + dictionary[randomRowIndex][language1Index];
 
         document.querySelector(".hint-1").textContent = hint1;
     });
