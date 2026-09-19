863dd06354187597af5fd9ea08aa86b2f7fb5469
HungryHippoNo3 <157853215+HungryHippoNo3@users.noreply.github.com>
2026-09-18 19:59:50 -0500

    Added Irish and Welsh

--- script.js
+++ script.js
@@ -8,13 +8,22 @@ window.addEventListener("DOMContentLoaded", () => {
         const noun = dictionary[randomRowIndex][0];
 
         const language1Index = (dictionary[0].length - 3) + Math.floor(Math.random() * 3);
+        const language2Index = (dictionary[0].length - 6) + Math.floor(Math.random() * 3);
+        const language3Index = (dictionary[0].length - 9) + Math.floor(Math.random() * 3);
+        const language4Index = (dictionary[0].length - 13) + Math.floor(Math.random() * 4);
+        const language5Index = (dictionary[0].length - 15) + Math.floor(Math.random() * 2);
+        const language6Index = (dictionary[0].length - 19) + Math.floor(Math.random() * 4);
+        const language7Index = (dictionary[0].length - 22) + Math.floor(Math.random() * 3);
+        const language8Index = (dictionary[0].length - 24) + Math.floor(Math.random() * 2);
 
         const hint1 = dictionary[0][language1Index] + ": " + dictionary[randomRowIndex][language1Index];
-
-        console.log("Random Row Index:", randomRowIndex);
-        console.log("Noun:", noun);
-        console.log("Language 1 Index:", language1Index);
-        console.log("Hint 1:", hint1);
+        const hint2 = dictionary[0][language2Index] + ": " + dictionary[randomRowIndex][language2Index];
+        const hint3 = dictionary[0][language3Index] + ": " + dictionary[randomRowIndex][language3Index];
+        const hint4 = dictionary[0][language4Index] + ": " + dictionary[randomRowIndex][language4Index];
+        const hint5 = dictionary[0][language5Index] + ": " + dictionary[randomRowIndex][language5Index];
+        const hint6 = dictionary[0][language6Index] + ": " + dictionary[randomRowIndex][language6Index];
+        const hint7 = dictionary[0][language7Index] + ": " + dictionary[randomRowIndex][language7Index];
+        const hint8 = dictionary[0][language8Index] + ": " + dictionary[randomRowIndex][language8Index];
 
         document.querySelector(".hint-1").textContent = hint1;
     });
