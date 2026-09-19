1606ba9c2daf8d0499c042fd12d7cad1c6afd9c1
HungryHippoNo3 <157853215+HungryHippoNo3@users.noreply.github.com>
2026-09-18 19:27:43 -0500

    Fixed small errors in language1index

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.addEventListener("DOMContentLoaded", () => {
         const randomRowIndex = 1 + Math.floor(Math.random() * (dictionary.length - 1));
         const noun = dictionary[randomRowIndex][0];
 
-        const language1Index = (dictionary.length() - 3) + Math.floor(Math.random() * 3);
+        const language1Index = (dictionary[0].length - 3) + Math.floor(Math.random() * 3);
 
         const hint1 = dictionary[0][language1Index] + ": " + dictionary[randomRowIndex][language1Index];
 
